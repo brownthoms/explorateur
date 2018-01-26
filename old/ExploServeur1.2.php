6c8d102423e9d5625e7f2c7f2c8fcce4782a61fe
magentall <magentallteller@gmail.com>
2018-01-26 16:39:19 +0100

    v.0.1.2.4

---
+++ old/ExploServeur1.2.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>infoserveur</title>
+    <script
+			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        function go(id){
+
+            //alert(id);
+            var id_clk ="#"+id;
+            //alert(id_clk);
+            var url = $(id_clk).text();
+            alert(url);
+
+        //$(document).ready(function() {
+          //$("#id_list_fct").change(function() {
+          //  setInterval(function(){
+            //  var namfctbox = $('#id_list_fct').find(":selected").val();
+
+
+              $.ajax({
+                type: 'post',
+                url: 'exploServ.1.2.php',
+                data: {url:url},//fct : namfctbox },
+                success: function(response,status){
+                  //alert(response);
+                  var a = JSON.parse(response);
+
+                  //alert(a[0]);
+                  //console.log(a[4]);
+                  $("#div1").empty();
+                  var tabDir=a[4];
+                  for (var vtab in tabDir) {
+                    //alert(vtab);
+                      $("#div1").append(tabDir[vtab]);
+                      $("#div1").append('<br>');
+                      console.log(tabDir[vtab]);
+                  }
+
+
+                  $("#div2").empty();
+                  $("#div3").empty();
+                  $("#divdate").empty();
+
+                  $("#div2").append("<pre>"+a[1]+"</pre>");
+                  $("#div3").append("<pre>"+a[2]+"</pre>");
+                  $("#divdate").append(a[3]);
+                }
+              });
+          //  },1000);
+        //  });
+      //  });
+    }
+    </script>
+  </head>
+  <body>
+<div class="container-fluid">
+  <div class="row">
+
+    <div class="col-3" id="div1"></div>
+    <div class="col-3" id="div2"></div>
+    <div class="col-3" id="div3"></div>
+    <div class="col-3" id="div4"><a id="tp1" value="/home/" onClick="go(this.id)">/home/</a></div>
+
+  </div>
+
+</div>
+  </body>
+</html>
